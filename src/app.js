440da872ccf80795e552de616baa7e935facee1c
ianfox1983 <ianfox1983@gmail.com>
2021-04-23 11:07:35 +0000

    Card finish

--- src/app.js
+++ src/app.js
@@ -2,10 +2,56 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const NUMEROS = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "J",
+  "Q",
+  "K",
+  "<img src='https://media.istockphoto.com/vectors/dancing-joker-with-playing-cards-on-white-vector-id960303126' style='width: 250px; height: 250px;'>"
+];
+const PALOS = ["♥", "♠", "♦", "♣"];
+const COLORES = ["black", "red"];
+
+const SimboloArriba = document.querySelector("#SimboloArriba");
+const numero = document.querySelector("#numero");
+const SimboloAbajo = document.querySelector("#SimboloAbajo");
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  GeneradorValor();
+  document.querySelector("#boton").addEventListener("click", event => {
+    // Al hacer click hace un a nueva llamada y genera un nuevo valor
+    GeneradorValor();
+  });
 };
+
+function GeneradorNumAleatorio(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+function GeneradorValor() {
+  let Simbolo = GeneradorNumAleatorio(PALOS);
+  let Color = GeneradorNumAleatorio(COLORES);
+  let Numeros = GeneradorNumAleatorio(NUMEROS);
+  SimboloArriba.innerHTML = Simbolo;
+  SimboloArriba.style.color = Color;
+  SimboloAbajo.innerHTML = Simbolo;
+  SimboloAbajo.style.color = Color;
+
+  numero.innerHTML = Numeros;
+
+  if (
+    Numeros ==
+    "<img src='https://media.istockphoto.com/vectors/dancing-joker-with-playing-cards-on-white-vector-id960303126' style='width: 250px; height: 250px;'>"
+  ) {
+    SimboloArriba.innerHTML = "";
+    SimboloAbajo.innerHTML = "";
+  }
+}
